7c7a05046348617f50ae896f2182e5aacc351bdf
Lizzy <ltenganah@unal.edu.co>
2017-08-17 20:29:48 +0000

    clase 2- nuevas vistas y controladores

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
     
     def index
         puts 'Hola mundo en consola'
+        @nombre = "Lizzy"
+        @imprimir = false
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'index/about'
+
+  get 'index/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # get '/welcome', to: 'welcome#index'
   root 'welcome#index'
+  
+  get '/about', to: 'index#about'
+  get '/contact', to: 'index#contact'
   
 end
